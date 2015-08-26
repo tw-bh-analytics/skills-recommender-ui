ba58c059f71b80cddc4d0ee5f8c6809ab45e59a8
Fabio Lima Santos <flsusp@gmail.com>
2015-08-25 21:32:59 -0300

    Autocomplete

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('skillsRecommenderUi', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ngRoute', 'mm.foundation']);
+    .module('skillsRecommenderUi', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ngRoute', 'mm.foundation', 'angucomplete-alt']);
 
 })();
