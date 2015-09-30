e377cdc1dbf56869e833c268071c3f5ba3f1c62b
Fabio Lima Santos <flsusp@gmail.com>
2015-09-30 09:48:32 -0300

    Adding the feedback buttons

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,6 +9,14 @@
   function MainController($interval, $scope, $log, Restangular) {
     $scope.person = null;
 
+    $scope.changedFeedbacks = function() {
+      $scope.recommendation.forEach(function(r) {
+        if (r.feedback !== null) {
+          Restangular.one('person', $scope.person.id).one('recommendation', r.id).put({ feedback: r.feedback });
+        }
+      });
+    };
+
     $scope.personFn = function(selected) {
       $scope.person = selected.originalObject;
       if ($scope.person !== null) {
